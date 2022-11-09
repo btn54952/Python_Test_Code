b430fdb73b8955468c16316917f82c08c92a6e27
Toby Brookes <Toby.Brookes@stfc.ac.uk>
2022-11-09 10:27:49 +0000

    Added the feature for users to input their own person data

--- Simple_Code.py
+++ Simple_Code.py
@@ -1,3 +1,3 @@
-Name = "David"
-Age = 43
+Name = input("please enter the name of the person")
+Age = input("please enter the age of the person")
 print("The person is called " , Name , "and they are " , Age , "years old")
